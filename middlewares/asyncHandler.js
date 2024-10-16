7d12a9c67179375e28e877a7b18adfbef281ce9b
Sternerr <volpy2000@gmail.com>
2024-10-16 12:05:17 +0200

    Asynchrnous Handle Middleware

---
+++ middlewares/asyncHandler.js
@@ -0,0 +1,7 @@
+// Middlware that wraps asynchronous middleware functions
+const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+}
+
+
+module.exports = asyncHandler;
